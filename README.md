904864d8754c75411a238939f2f7636f0653516c
Joshbrar <brarjoshua@gmail.com>
2020-09-22 19:07:20 -0700

    The final work done is updated
    Updated VB script file
    Added images folder that contans screen shots from the main excel documents when the macros are executed.
    Also the VBA is added on the Multiple_year_stock_data.xlsx
    
    The readme.md file has been updated that contains documentation for this project.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # VBA-Scripting
 Stock Market Analysis 
+
+
+
+# A project VBA-challenge was created.
+
+#There are three excel files that were used during the coding and development and testing and debgging phases. 
+
+	# There files are 
+	  Alphabetical_Development.xlsm
+	  alphabetical_testing.xlsm
+	  Multiple_year_stock_data.xlsm  
+ 
+ 	# alphabetical_testing.xlsx and  Multiple_year_stock_data.xlsx were downloaded from GITLAB VBA-Challege homework folder. 
+	
+	# There two files were convert to alphabetical_testing.xlsm and Multiple_year_stock_data.xlsm respectively as the 
+	
+	  VBA is run only on files with extension .xlsm file types
+	  
+	# The VBA coding for the project is in the file  VB_Script.vb 
+
+#  The script loops through all the stocks for one year and output the following information.
+
+  * The ticker symbol.
+
+  * Yearly change from opening price at the beginning of a given year to the closing price at the end of that year.
+
+  * The percent change from opening price at the beginning of a given year to the closing price at the end of that year.
+
+  * The total stock volume of the stock.
+
+* Additional conditional formatting  has been done that highlights positive changes in green and negative changes in red.
+
+# A number of screenshots have taken from our main file (multiple_year_stock_data.xlsm )  for the audience to view them. Those
+  screen shots are in the folder images. 
+
+
+# CHALLENGES
+
+  The macros "runAll" automates the script that performas calculations for all the worksheets in the Stock Analysis Data file. 
+  
+  
+  
+  
+
+	  
+	
--- VB_Script.vb
+++ VB_Script.vb
@@ -1,12 +1,31 @@
 
+'Code to run for all worksheets
+
+Sub runAll()
+    Dim xSh As Worksheet
+    Application.ScreenUpdating = False
+    For Each xSh In Worksheets
+        xSh.Select
+        Call fn_Ticker
+    Next
+    Application.ScreenUpdating = True
+End Sub
+
+
+' This function will do all the calculations and display the readings
+
 Sub fn_Ticker()
 
 Dim lastRow As Long
- lastRow = Cells(Rows.Count, 1).End(xlUp).Row
- MsgBox ("last row for Year " & lastRow)
 
-'Loop through rows for each year
- TickerYear = "2014"
+
+
+'get the last row of the worksheet
+
+ lastRow = Cells(Rows.Count, 1).End(xlUp).Row
+  ' MsgBox ("last row for Year " & lastRow)  ' read the data to process and displaytemp
+
+'assign variables
 
 currentTickerTtlCtr = 0
 currentTickerName = ""
@@ -16,7 +35,17 @@ NewTicker = "Yes"
 currentTotalTickerStockVol = 0
 CntPrintTicker = 2
 
+'Print Output Headers
+       Cells(1, 13).Value = "Ticker"
+       Cells(1, 14).Value = "Yearly Change"
+       Cells(1, 15).Value = "Change Percentage"
+       Cells(1, 16).Value = "Total Stock Volume"
+       
+       '  Cells(1, 17).Value = "openValue"   ' for debugging
+       '  Cells(1, 18).Value = "CloseValue"   ' for debugging
+
 For y = 2 To lastRow
+
       currentTickerName = Cells(y, 1).Value
       NextRowTickerName = Cells(y + 1, 1).Value
       If currentTickerName = NextRowTickerName Then
@@ -34,24 +63,33 @@ For y = 2 To lastRow
       
        currentTotalTickerStockVol = currentTotalTickerStockVol + Cells(y, 7).Value
        Cells(CntPrintTicker, 13).Value = currentTickerName
-                     
-       Cells(CntPrintTicker, 14).Value = currentTickerOpenValue
-       Cells(CntPrintTicker, 15).Value = currentTickerCloseValue
-       Cells(CntPrintTicker, 18).Value = currentTotalTickerStockVol
+      
+      'Cells(CntPrintTicker, 14).Value = currentTickerOpenValue
+      ' Cells(CntPrintTicker, 15).Value = currentTickerCloseValue
+       
+       Cells(CntPrintTicker, 16).Value = currentTotalTickerStockVol
        YearlyChange = currentTickerCloseValue - currentTickerOpenValue
        
-       Cells(CntPrintTicker, 16).Value = YearlyChange
+       Cells(CntPrintTicker, 14).Value = YearlyChange
+       
+       ' code for changing color of cell if yearly change is positive or negative
        
        If YearlyChange >= 0 Then
-            Cells(CntPrintTicker, 16).Interior.ColorIndex = 4
+            Cells(CntPrintTicker, 14).Interior.ColorIndex = 4
        Else
-            Cells(CntPrintTicker, 16).Interior.ColorIndex = 3
+            Cells(CntPrintTicker, 14).Interior.ColorIndex = 3
        End If
-       
+         
+         ' Make sure the TickerOpen Value is not zero   - question for Instructor and TA
+         If currentTickerOpenValue > 0 Then
            percent_Chg = (currentTickerCloseValue - currentTickerOpenValue) / currentTickerOpenValue
  
-           Cells(CntPrintTicker, 17).Value = percent_Chg
-       
+           'Cells(CntPrintTicker, 15).Value = percent_Chg * 100 & "%"
+              Cells(CntPrintTicker, 15).Value = FormatPercent(percent_Chg, , , , vbFalse)
+         Else
+         End If
+         
+           
        CntPrintTicker = CntPrintTicker + 1
        currentTotalTickerStockVol = 0
        currentTickerOpenValue = ""
@@ -64,11 +102,17 @@ For y = 2 To lastRow
 
 Next y
 
+' GetMaxpercentage MinPercentage , Max Total Stock Values
+
+' Cells(5, 17).Value = "Greatest Total Stock Value :  "
+' Cells(5, 18).Value = Application.WorksheetFunction.Max(Range("P:P"))
+'lastRowBonus = Cells(Rows.Count, 13).End(xlUp).Row
+'Cells(5, 17).Value = "Last Row Bonus Work  "
+' Cells(5, 18).Value = lastRowBonus
  
-
-
+ 'For varYY = 2 To lastRowBonus
+ 
+ 
+ 'Next varYY
+ 
 End Sub
-
-
-
-
